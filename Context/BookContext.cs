b157299c5c1da42966234babdcd3ddf857f7259b
thanh thao <dothithanhthao24091980@outlook.com>
2020-04-02 21:29:29 +0700

    Add project files.

---
+++ Context/BookContext.cs
@@ -0,0 +1,14 @@
+﻿using ASP.NETMVCLambdaExpressions.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NETMVCLambdaExpressions.Context
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
